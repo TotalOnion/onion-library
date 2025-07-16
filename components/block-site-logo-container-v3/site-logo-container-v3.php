2ab8a7966db46c27b250cc06eb2aea2a846d2fb9
Ciaran Kamp <ciarankamp@gmail.com>
2025-07-16 12:29:21 +0100

    library

---
+++ components/block-site-logo-container-v3/site-logo-container-v3.php
@@ -0,0 +1,14 @@
+<?php
+
+acf_register_block_type(
+	array(
+		'name'            => 'site-logo-container-v3',
+		'title'           => __('Site logo container v3', 'Global-theme Admin'),
+		'render_callback' => 'athena_block_render_post_object',
+		'category'        => 'common',
+		'icon'            => get_svg_icon_content('brick.svg'),
+		'keywords'        => array('content', 'text'),
+		'mode'            => 'preview',
+		'supports'        => array('align' => false, 'anchor' => true),
+	)
+);
