151e9ccb8a9aaaac0a47d7cba8742fff039c2327
robertbarry73 <robertbarry101@live.com>
2025-07-18 15:45:23 +0100

    Market selector v3 js

--- components/block-market-selector-v3/market-selector-v3.js
+++ components/block-market-selector-v3/market-selector-v3.js
@@ -1,7 +1,25 @@
-export default function marketselectorv3Js ( options = {} ) {
+export default function marketselectorv3Js(options = {}) {
 	try {
-		const { block } = options;
-	} catch ( error ) {
-		console.error( error );
+		const {block} = options;
+		const marketSelectorPopupBtn = block.querySelector('.pop-up');
+		const marketSelectorInlineBtn = block.querySelector('.inline');
+		const marketSelectorModal = block.querySelector(
+			'.market-selector-v3__modal-overlay'
+		);
+		const marketSelectorModalClose = block.querySelector(
+			'.market-selector-v3__modal-close-button'
+		);
+		if (marketSelectorPopupBtn) {
+			marketSelectorPopupBtn.addEventListener('click', (e) => {
+				marketSelectorModal.style.display = 'flex';
+			});
+		}
+		if (marketSelectorModalClose) {
+			marketSelectorModalClose.addEventListener('click', (e) => {
+				marketSelectorModal.style.display = 'none';
+			});
+		}
+	} catch (error) {
+		console.error(error);
 	}
 }
