8b293e017fa17c59716673c3a5695c54edc10bc1
Ethan Brackstone <dev.brackstone@gmail.com>
2025-07-21 15:32:15 +0100

    block-interactions & component-block-settings

---
+++ components/block-block-interactions-v3/block-interactions-v3.js
@@ -0,0 +1,46 @@
+export default function blockinteractionsv3Js(options = {}) {
+	try {
+		const { block } = options;
+		
+		const blockId = block.dataset.blockid;
+		const parentBlockId = block.dataset.parentblockid;
+
+		const blockCloseButtons = block.querySelectorAll('.block-interactions-v3__block-close-button');
+		if (!blockCloseButtons.length) {
+			return;
+		}
+
+		blockCloseButtons.forEach(button => {
+			const shouldStoreCookie = button?.dataset?.storeCookie === 'true';
+			const cookieKey = `blockClosed_${parentBlockId}`;
+			const wrappingSection = document.querySelector(`[data-blockid="${parentBlockId}"]`);
+			
+			if (getCookie(cookieKey) === 'true') {
+				wrappingSection.remove();
+				return;
+			}
+
+			button.addEventListener('click', () => {
+				wrappingSection.remove();
+				if (shouldStoreCookie) {
+					setCookie(cookieKey, 'true', 7);
+				}
+			});
+
+		});
+
+
+		function setCookie(name, value, days) {
+			const expires = new Date(Date.now() + days * 864e5).toUTCString();
+			document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+		}
+		function getCookie(name) {
+			return document.cookie.split('; ').reduce((result, cookieString) => {
+				const [key, val] = cookieString.split('=');
+				return key === name ? decodeURIComponent(val) : result;
+			}, '');
+		}
+	} catch (error) {
+		console.error('Error in blockinteractionsv3Js:', error);
+	}
+}
