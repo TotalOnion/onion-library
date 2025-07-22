7b4906bd1038aa861acca51e0ce5ce68e7697b46
robertbarry73 <robertbarry101@live.com>
2025-07-22 12:00:34 +0100

    Form selection v3

---
+++ components/block-form-selection-v3/form-selection-v3.php
@@ -0,0 +1,14 @@
+<?php
+
+acf_register_block_type(
+	array(
+		'name'            => 'form-selection-v3',
+		'title'           => __( 'Form selection v3', 'Global-theme Admin' ),
+		'render_callback' => 'athena_block_render_post_object',
+		'category'        => 'common',
+		'icon'            => get_svg_icon_content('brick.svg'),
+		'keywords'        => array('content', 'text' ),
+		'mode'            => 'preview',
+		'supports'        => array( 'align' => false, 'anchor' => true ),
+	)
+);
