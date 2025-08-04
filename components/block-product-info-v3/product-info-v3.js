749f94f81d19ec44cbce22e6d9925bc8113fe712
Ciaran Kamp <ciarankamp@gmail.com>
2025-08-04 16:01:39 +0100

    product info

--- components/block-product-info-v3/product-info-v3.js
+++ components/block-product-info-v3/product-info-v3.js
@@ -1,7 +1,10 @@
-export default function productinfov3Js ( options = {} ) {
+// LIBRARY FILE - Do not modify/override here as your changes will be lost when the package is updated.
+import extraJs from 'Assets/js/blocks/product-info-v3/post-info-v3-extra';
+export default function productinfov3Js(options = {}) {
+	const {block} = options;
 	try {
-		const { block } = options;
-	} catch ( error ) {
-		console.error( error );
+		extraJs(block);
+	} catch (error) {
+		console.error(error);
 	}
 }
--- components/block-product-info-v3/product-info-v3.php
+++ components/block-product-info-v3/product-info-v3.php
@@ -3,12 +3,15 @@
 acf_register_block_type(
 	array(
 		'name'            => 'product-info-v3',
-		'title'           => __( 'Product info v3', 'Global-theme Admin' ),
+		'title'           => __('Product info v3', 'Global-theme Admin'),
 		'render_callback' => 'athena_block_render_post_object',
 		'category'        => 'common',
 		'icon'            => get_svg_icon_content('brick.svg'),
-		'keywords'        => array('content', 'text' ),
+		'keywords'        => array('content', 'text'),
 		'mode'            => 'preview',
-		'supports'        => array( 'align' => false, 'anchor' => true ),
+		'supports'        => array(
+			'align'  => false,
+			'anchor' => true,
+		),
 	)
 );
