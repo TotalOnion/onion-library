d83d1d55c11b4831736a3e59603b9174c83699ba
Ciaran Kamp <ciarankamp@gmail.com>
2025-08-04 16:32:46 +0100

    prod info

--- components/block-product-info-v3/product-info-v3.js
+++ components/block-product-info-v3/product-info-v3.js
@@ -1,5 +1,5 @@
 // LIBRARY FILE - Do not modify/override here as your changes will be lost when the package is updated.
-import extraJs from 'Assets/js/blocks/product-info-v3/post-info-v3-extra';
+import extraJs from 'Assets/js/blocks/product-info-v3/product-info-v3-extra';
 export default function productinfov3Js(options = {}) {
 	const {block} = options;
 	try {
