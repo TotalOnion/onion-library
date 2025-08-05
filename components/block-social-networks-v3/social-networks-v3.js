5ec1f30bba38c73e0f188adcb0acefbd410e51bc
adrianfti <adrianfti12@gmail.com>
2025-08-05 15:53:54 +0100

    social networks/post info/product info

---
+++ components/block-social-networks-v3/social-networks-v3.js
@@ -0,0 +1,7 @@
+export default function socialnetworksv3Js ( options = {} ) {
+	try {
+		const { block } = options;
+	} catch ( error ) {
+		console.error( error );
+	}
+}
---
+++ components/block-social-networks-v3/social-networks-v3.php
@@ -0,0 +1,14 @@
+<?php
+
+acf_register_block_type(
+	array(
+		'name'            => 'social-networks-v3',
+		'title'           => __( 'Social networks v3', 'Global-theme Admin' ),
+		'render_callback' => 'athena_block_render_post_object',
+		'category'        => 'common',
+		'icon'            => get_svg_icon_content('brick.svg'),
+		'keywords'        => array('content', 'text' ),
+		'mode'            => 'preview',
+		'supports'        => array( 'align' => false, 'anchor' => true ),
+	)
+);
