ed6f0768f79b7f04233cdffce6d46c9b8ecdd365
Ciaran Kamp <ciarankamp@gmail.com>
2025-08-28 14:23:16 +0100

    import updates

--- components/block-market-selector-v3/market-selector-v3.js
+++ components/block-market-selector-v3/market-selector-v3.js
@@ -1,26 +1,6 @@
 export default function marketselectorv3Js(options = {}) {
 	try {
 		const {block} = options;
-		const marketSelectorPopupBtn = block.querySelector('.pop-up');
-		const marketSelectorInlineBtn = block.querySelector('.inline');
-		const marketSelectorModal = block.querySelector(
-			'.market-selector-v3__modal-overlay'
-		);
-		const marketSelectorModalClose = block.querySelector(
-			'.market-selector-v3__modal-close-button'
-		);
-		if (marketSelectorPopupBtn) {
-			marketSelectorPopupBtn.addEventListener('click', (e) => {
-				marketSelectorModal.style.display = 'flex';
-				document.documentElement.classList.add('lock-position');
-			});
-		}
-		if (marketSelectorModalClose) {
-			marketSelectorModalClose.addEventListener('click', (e) => {
-				marketSelectorModal.style.display = 'none';
-				document.documentElement.classList.remove('lock-position');
-			});
-		}
 	} catch (error) {
 		console.error(error);
 	}
