2c0df16dea285ead3fb0d7869e7e69850ded9776
Ciaran Kamp <ciarankamp@gmail.com>
2025-09-01 20:02:14 +0100

    editor

--- components/fields-block-editor-assets-v3/block-editor-assets-v3.php
+++ components/fields-block-editor-assets-v3/block-editor-assets-v3.php
@@ -3,9 +3,9 @@ add_action('enqueue_block_assets', 'add_block_editor_assets_v3', 10, true);
 function add_block_editor_assets_v3(): void
 {
     if (is_admin()) {
-        wp_enqueue_style('core-wordpress-block-editor-previews', gtp_get_asset_uri('corewordpressblockeditorpreviewcss.css'), array(), null);
+        wp_enqueue_style('block-editor-preview', gtp_get_asset_uri('blockeditorpreviewcss.css'), array(), null);
         wp_enqueue_script('editorstyles', gtp_get_asset_uri('corewordpressblockeditorpreview.js'), array(), null, true);
-        wp_localize_script('editorstyles', 'previewvars', core_design_system());
+        wp_localize_script('editorstyles', 'previewvars', core_design_system_v3());
         wp_localize_script('editorstyles', 'corecta', core_cta());
         wp_localize_script('editorstyles', 'coretypography', core_typography_v3());
         wp_localize_script('editorstyles', 'corefontmodifiers', core_font_modifiers());
