e01833e6066fd4e277156ee7bb07b6c2fa321aee
Ciaran Kamp <ciarankamp@gmail.com>
2025-09-07 14:52:41 +0200

    nav and group

--- components/block-group-container-v3/group-container-v3.js
+++ components/block-group-container-v3/group-container-v3.js
@@ -1,6 +1,8 @@
+import extraJs from 'Assets/js/blocks/group-container-v3/group-container-v3-extra.js';
 export default function groupcontainerv3Js(options = {}) {
 	try {
 		const {block} = options;
+		extraJs(block);
 	} catch (error) {
 		console.error(error);
 	}
--- components/block-nav-menu-container-v3/nav-menu-container-v3.js
+++ components/block-nav-menu-container-v3/nav-menu-container-v3.js
@@ -1,16 +1,9 @@
-// import extraJs from 'Assets/js/blocks/nav-menu-container-v3/nav-menu-container-v3-extra';
+import extraJs from 'Assets/js/blocks/nav-menu-container-v3/nav-menu-container-v3-extra';
 
 export default function navmenucontainerv3Js(options = {}) {
 	try {
 		const {block} = options;
-		if (block.dataset.navtype == 'slide_in_nav_panel') {
-			// import(
-			// 	'Assets/js/modules/library-modules/nav-menu-slide-in-panel/nav-menu-slide-in-panel.js'
-			// ).then((module) => {
-			// 	module.default({block});
-			// });
-		}
-		// extraJs(block);
+		extraJs(block);
 	} catch (error) {
 		console.error(error);
 	}
