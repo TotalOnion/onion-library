a953df066d68cda2af22d2a98d47f9a8354f001e
adrianfti <adrianfti12@gmail.com>
2025-09-15 15:29:26 +0100

    post info v3

--- components/block-post-info-v3/post-info-v3.js
+++ components/block-post-info-v3/post-info-v3.js
@@ -1,7 +1,10 @@
 import extraJs from 'Assets/js/blocks/post-info-v3/post-info-v3-extra';
+import videocontroller from '@total_onion/onion-videocontroller/onion-videocontroller.js';
+
 export default function postinfov3Js(options = {}) {
 	const {block} = options;
 	try {
+		const videoController = new videocontroller(block);
 		extraJs(block);
 	} catch (error) {
 		console.error(error);
