c6d91c6946af1b694da906a896fb66d510407a72
Ciaran Kamp <ciarankamp@gmail.com>
2025-10-14 09:30:45 +0100

    image gallery

--- components/block-featured-image-gallery-v3/featured-image-gallery-v3.js
+++ components/block-featured-image-gallery-v3/featured-image-gallery-v3.js
@@ -1,26 +1,20 @@
-import {fadeIn} from '@pernod-ricard-global-cms/jsanimations';
-import {loadCss} from '@pernod-ricard-global-cms/jsutils';
-
 export default function featuredImagegalleryJs(options = {}) {
 	try {
 		const {block} = options;
-		loadCss(block.dataset.assetkey, options).then(() => {
-			fadeIn(block);
-			const featuredImage = block.querySelector(
-				`.featured-image-gallery-v3__featured-image`
-			);
-			const otherImages = block.querySelectorAll(
-				`.featured-image-gallery-v3__image`
-			);
-			otherImages.forEach((image) => {
-				image.addEventListener('click', function updateSrc() {
-					featuredImage.setAttribute(
-						'srcset',
-						this.getAttribute('srcset')
-					);
-					featuredImage.setAttribute('src', this.getAttribute('src'));
-					featuredImage.setAttribute('alt', this.getAttribute('alt'));
-				});
+		const featuredImage = block.querySelector(
+			`.featured-image-gallery-v3__featured-image`
+		);
+		const otherImages = block.querySelectorAll(
+			`.featured-image-gallery-v3__image`
+		);
+		otherImages.forEach((image) => {
+			image.addEventListener('click', function updateSrc() {
+				featuredImage.setAttribute(
+					'srcset',
+					this.getAttribute('srcset')
+				);
+				featuredImage.setAttribute('src', this.getAttribute('src'));
+				featuredImage.setAttribute('alt', this.getAttribute('alt'));
 			});
 		});
 	} catch (error) {
