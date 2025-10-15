706b646fc52d4329bf1f4c4ea5c993607a6bcdfc
Ciaran Kamp <ciarankamp@gmail.com>
2025-10-15 11:21:35 +0100

    featured

--- components/block-featured-image-gallery-v3/featured-image-gallery-v3.js
+++ components/block-featured-image-gallery-v3/featured-image-gallery-v3.js
@@ -7,18 +7,32 @@ export default function featuredImagegalleryJs(options = {}) {
 		const featuredImage = block.querySelector(
 			`.featured-image-gallery-v3__featured-image`
 		);
-		const otherImages = block.querySelectorAll(
-			`.featured-image-gallery-v3__image`
+		const thumbnailImages = block.querySelectorAll(
+			`.featured-image-gallery-v3__image-wrapper`
 		);
-		otherImages.forEach((image) => {
+		thumbnailImages.forEach((image) => {
 			image.addEventListener('click', function updateSrc() {
+				thumbnailImages.forEach((wrapper) => {
+					wrapper.classList.remove(
+						'featured-image-gallery-v3__image-wrapper--active'
+					);
+				});
+				image.classList.add(
+					'featured-image-gallery-v3__image-wrapper--active'
+				);
 				featuredImageContainer.classList.add('changing-image');
 				setTimeout(() => {
-					featuredImage.setAttribute('src', this.getAttribute('src'));
-					featuredImage.setAttribute('alt', this.getAttribute('alt'));
+					featuredImage.setAttribute(
+						'src',
+						this.querySelector('img').getAttribute('src')
+					);
+					featuredImage.setAttribute(
+						'alt',
+						this.querySelector('img').getAttribute('alt')
+					);
 					featuredImage.setAttribute(
 						'srcset',
-						this.getAttribute('srcset')
+						this.querySelector('img').getAttribute('srcset')
 					);
 					featuredImageContainer.classList.remove('changing-image');
 				}, 800);
