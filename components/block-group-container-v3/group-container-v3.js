4800d0bc8b155f8117f6f13a2a90affb9f2d2098
Ciaran Kamp <ciarankamp@gmail.com>
2025-10-20 14:11:22 +0100

    tidy

--- components/block-group-container-v3/group-container-v3.js
+++ components/block-group-container-v3/group-container-v3.js
@@ -1,14 +1,6 @@
 export default function groupcontainerv3Js(options = {}) {
 	try {
 		const {block} = options;
-
-		// if (document.documentElement.dataset.athenadevelopment == 'true') {
-		// 	import(
-		// 		'Assets/js/blocks/group-container-v3/group-container-v3-extra.js'
-		// 	).then((result) => {
-		// 		result.default({block: block});
-		// 	});
-		// }
 	} catch (error) {
 		console.error(error);
 	}
