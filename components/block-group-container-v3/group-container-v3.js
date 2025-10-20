60bf392cbddd63f242f9f61cb2ed64a1c206f64a
Ciaran Kamp <ciarankamp@gmail.com>
2025-10-20 13:57:58 +0100

    remove group container v3 js extra

--- components/block-group-container-v3/group-container-v3.js
+++ components/block-group-container-v3/group-container-v3.js
@@ -2,13 +2,13 @@ export default function groupcontainerv3Js(options = {}) {
 	try {
 		const {block} = options;
 
-		if (document.documentElement.dataset.athenadevelopment == 'true') {
-			import(
-				'Assets/js/blocks/group-container-v3/group-container-v3-extra.js'
-			).then((result) => {
-				result.default({block: block});
-			});
-		}
+		// if (document.documentElement.dataset.athenadevelopment == 'true') {
+		// 	import(
+		// 		'Assets/js/blocks/group-container-v3/group-container-v3-extra.js'
+		// 	).then((result) => {
+		// 		result.default({block: block});
+		// 	});
+		// }
 	} catch (error) {
 		console.error(error);
 	}
