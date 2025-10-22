b20936c53feea7fa70a239a45aace1df5d3d0e4e
abdouelmes <a.elmes0@gmail.com>
2025-10-22 16:15:19 +0100

    new spotify block

---
+++ components/block-spotify-embed-v3/spotify-embed-v3.php
@@ -0,0 +1,14 @@
+<?php
+
+acf_register_block_type(
+	array(
+		'name'            => 'spotify-embed-v3',
+		'title'           => __( 'Spotify embed v3', 'Global-theme Admin' ),
+		'render_callback' => 'athena_block_render_post_object_v3',
+		'category'        => 'common',
+		'icon'            => get_svg_icon_content('brick.svg'),
+		'keywords'        => array('content', 'text' ),
+		'mode'            => 'preview',
+		'supports'        => array( 'align' => false, 'anchor' => true ),
+	)
+);
