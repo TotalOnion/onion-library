86c66e4317262cbaf1182067bbdb7a2806b81587
Ciaran Kamp <ciarankamp@gmail.com>
2025-10-27 17:51:49 +0000

    restore some css and js

---
+++ components/block-standard-content-v3/standard-content-v3.js
@@ -0,0 +1,39 @@
+export default function standardcontentv3Js(options = {}) {
+	try {
+		const {block} = options;
+		// const formContainerElement = block.querySelector('.form-container');
+		// const modalFormContainerElement = block.querySelector(
+		// 	'.form-container__modal'
+		// );
+		// const countdownElement = block.querySelector('.countdowndate');
+		// const socialMediaElement = block.querySelector(
+		// 	'.social-media-share-popup'
+		// );
+
+		// if (formContainerElement || modalFormContainerElement) {
+		// 	import(
+		// 		'Assets/js/modules/library-modules/form-selection/form-selection.js'
+		// 	).then((module) => {
+		// 		module.default(block);
+		// 	});
+		// }
+
+		// if (countdownElement) {
+		// 	import(
+		// 		'Assets/js/modules/library-modules/countdown/countdown.js'
+		// 	).then((module) => {
+		// 		module.default(block);
+		// 	});
+		// }
+
+		// if (socialMediaElement) {
+		// 	import(
+		// 		'Assets/js/modules/library-modules/social-media-share/social-media-share.js'
+		// 	).then((module) => {
+		// 		module.default(block);
+		// 	});
+		// }
+	} catch (error) {
+		console.error(error);
+	}
+}
---
+++ components/block-sub-group-container-v3/sub-group-container-v3.js
@@ -0,0 +1,9 @@
+// LIBRARY FILE - Do not modify/override here as your changes will be lost when the package is updated.
+export default function subgroupcontainerv3Js(options = {}) {
+	// try {
+	// 	const {block} = options;
+	// 	loadCss(block.dataset.assetkey, options).then(() => {});
+	// } catch (error) {
+	// 	console.error(error);
+	// }
+}
