f8fe20c8d8159cc7b9ce7c2bef8c7ca7d4151a97
Ciaran Kamp <ciarankamp@gmail.com>
2025-11-03 18:39:57 +0000

    spacer

--- components/block-post-type-filter-grid-v3/post-type-filter-grid-v3/spacer-widget.vue
+++ components/block-post-type-filter-grid-v3/post-type-filter-grid-v3/spacer-widget.vue
@@ -7,7 +7,7 @@ const gridLayoutString = gridLayoutElement(props);
 const blockClassname = "post-type-filter-grid-v3";
 </script>
 <template>
-    <div :class="`${blockClassname}__post-spacer ${blockClassname}__info-item`"
+    <div :class="`${blockClassname}__spacer ${blockClassname}__info-item`"
         :style="`${gridLayoutString}; --spacer-size-mult:${element.spacer_size}; --spacer-size-mult-portrait:${element.size_portrait}; --spacer-size-mult-mobile:${element.size_mobile}; --spacer-bg-colour:${element.spacer_background}`">
     </div>
 </template>
