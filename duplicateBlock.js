34d638b6e1b4146aab4a61c5396cf6bc51bdfd33
Ciaran Kamp <ciarankamp@gmail.com>
2026-04-01 15:15:41 +0100

    duplicate block

--- duplicateBlock.js
+++ duplicateBlock.js
@@ -1,27 +1,30 @@
 require('dotenv').config();
 const fs = require('fs');
-const { globSync } = require('glob');
+const path = require('path');
+const {globSync} = require('glob');
 const yargs = require('yargs');
 const compressing = require('compressing');
-const { log } = require('console');
 const templateOptions = yargs.argv._;
 const themePath =
 	process.env.THEME_PATH || 'web/wp-content/themes/global-theme';
 const srcPathJs = `${themePath}/assets/js/blocks`;
 const srcPathScss = `${themePath}/assets/scss/blocks`;
 const srcPathTwig = `${themePath}/views/blocks`;
-const srcPathPhp = `${themePath}/inc/acf-blocks`;
-const srcPathVue = `${themePath}/assets/vue/blocks`;
-const destPath = '../../../../exported-blocks';
+const srcPathBlockJson = `${themePath}/inc/acf-blocks`;
+// exported-blocks lives at the project root, relative to cwd (the theme dir)
+const destPath =
+	process.env.EXPORTED_BLOCKS_PATH ||
+	path.resolve(process.cwd(), '../../../../exported-blocks');
 const newName = templateOptions[1];
 
-const exisitingBlockNames = globSync(`${themePath}/assets/js/blocks/*.js`)
-	.map((path) => {
-		const assetKey = path
-			.replace('assets/js/blocks/', '')
-			.replace('.js', '');
-		return assetKey;
-	});
+const exisitingBlockNames = [
+	...globSync(`${themePath}/assets/js/blocks/*.js`).map((p) =>
+		path.basename(p, '.js')
+	),
+	...globSync(`${themePath}/inc/acf-blocks/*/block.json`).map((p) =>
+		path.basename(path.dirname(p))
+	)
+];
 
 const blockName = templateOptions[0];
 
@@ -54,162 +57,72 @@ if (exisitingBlockNames.includes(blockName)) {
 	}
 	fs.mkdirSync(dir);
 	if (fs.existsSync(`${srcPathJs}/${blockName}.js`)) {
-		fs.readFile(
+		const contents = fs.readFileSync(
 			`${srcPathJs}/${blockName}.js`,
-			'utf-8',
-			(err, contents) => {
-				if (err) throw err;
-				const regEx = RegExp(
-					String.raw`(${blockName.replaceAll(/( |-)/g, '')})`,
-					'gi'
-				);
-
-				const replaced = contents
-					.replaceAll(
-						regEx,
-						`${newName.toLowerCase().replaceAll(/( |-)/g, '')}`
-					)
-					.replaceAll(`blocks/${blockName}`, `blocks/${newName}`);
-				fs.writeFile(
-					`${dir}/${newName}.js`,
-					replaced,
-					'utf-8',
-					function (err) {
-						if (err) throw err;
-						console.log(
-							`👑👑\x1b[32m Successfully duplicated the js file! 👑👑`
-						);
-					}
-				);
-			}
+			'utf-8'
 		);
+		const regEx = RegExp(
+			String.raw`(${blockName.replaceAll(/( |-)/g, '')})`,
+			'gi'
+		);
+		const replaced = contents
+			.replaceAll(
+				regEx,
+				`${newName.toLowerCase().replaceAll(/( |-)/g, '')}`
+			)
+			.replaceAll(`blocks/${blockName}`, `blocks/${newName}`);
+		fs.writeFileSync(`${dir}/${newName}.js`, replaced, 'utf-8');
+		console.log(`👑👑\x1b[32m Successfully duplicated the js file! 👑👑`);
 	}
 	if (fs.existsSync(`${srcPathScss}/${blockName}.scss`)) {
-		fs.readFile(
+		const contents = fs.readFileSync(
 			`${srcPathScss}/${blockName}.scss`,
-			'utf-8',
-			(err, contents) => {
-				if (err) throw err;
-				const replaced = contents.replaceAll(
-					`${blockName}`,
-					`${newName}`
-				);
-				fs.writeFile(
-					`${dir}/${newName}.scss`,
-					replaced,
-					'utf-8',
-					function (err) {
-						if (err) throw err;
-						console.log(
-							`👑👑\x1b[32m Successfully duplicated the scss file! 👑👑`
-						);
-					}
-				);
-			}
+			'utf-8'
 		);
+		const replaced = contents.replaceAll(`${blockName}`, `${newName}`);
+		fs.writeFileSync(`${dir}/${newName}.scss`, replaced, 'utf-8');
+		console.log(`👑👑\x1b[32m Successfully duplicated the scss file! 👑👑`);
 	}
 	if (fs.existsSync(`${srcPathTwig}/${blockName}.twig`)) {
-		fs.readFile(
+		const contents = fs.readFileSync(
 			`${srcPathTwig}/${blockName}.twig`,
-			'utf-8',
-			(err, contents) => {
-				if (err) throw err;
-				const regEx = RegExp(String.raw`(\"${blockName}) `, 'gi');
-				const replaced = contents.replaceAll(regEx, `"${newName} `);
-				const regEx2 = RegExp(
-					String.raw`([\"|\']${blockName}[\"|\'])`,
-					'gi'
-				);
-				const replaced2 = replaced.replaceAll(regEx2, `"${newName}"`);
-				fs.writeFile(
-					`${dir}/${newName}.twig`,
-					replaced2,
-					'utf-8',
-					function (err) {
-						if (err) throw err;
-						console.log(
-							`👑👑\x1b[32m Successfully duplicated the twig file! 👑👑`
-						);
-					}
-				);
-			}
+			'utf-8'
+		);
+		const regEx = RegExp(String.raw`(\"${blockName}) `, 'gi');
+		const replaced = contents.replaceAll(regEx, `"${newName} `);
+		const regEx2 = RegExp(String.raw`([\"|\']${blockName}[\"|\'])`, 'gi');
+		const replaced2 = replaced.replaceAll(regEx2, `"${newName}"`);
+		fs.writeFileSync(`${dir}/${newName}.twig`, replaced2, 'utf-8');
+		console.log(`👑👑\x1b[32m Successfully duplicated the twig file! 👑👑`);
+	}
+	if (fs.existsSync(`${srcPathBlockJson}/${blockName}/block.json`)) {
+		const contents = fs.readFileSync(
+			`${srcPathBlockJson}/${blockName}/block.json`,
+			'utf-8'
+		);
+		const data = JSON.parse(contents);
+		data.name = `acf/${newName}`;
+		data.title = newName
+			.split('-')
+			.map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+			.join(' ');
+		if (data.description) {
+			data.description = data.description.replaceAll(blockName, newName);
+		}
+		fs.writeFileSync(
+			`${dir}/block.json`,
+			JSON.stringify(data, null, '\t'),
+			'utf-8'
+		);
+		console.log(
+			`👑👑\x1b[32m Successfully duplicated the block.json file! 👑👑`
 		);
 	}
-	if (fs.existsSync(`${srcPathPhp}/${blockName}.php`)) {
-		fs.readFile(
-			`${srcPathPhp}/${blockName}.php`,
-			'utf-8',
-			(err, contents) => {
-				if (err) throw err;
-				const replaced = contents.replaceAll(
-					`${blockName}`,
-					`${newName}`
-				);
-				const regEx = RegExp(
-					String.raw`${blockName.replaceAll('-', ' ')}`,
-					'gi'
-				);
-				const replaced2 = replaced.replaceAll(
-					regEx,
-					`${newName.slice(0, 1).toUpperCase()}${newName
-						.replaceAll('-', ' ')
-						.slice(1)}`
-				);
-				const regEx2 = RegExp(
-					String.raw`${blockName.replaceAll('-', '_')}`,
-					'gi'
-				);
-				const replaced3 = replaced2.replaceAll(
-					regEx2,
-					`${newName.replaceAll('-', '_')}`
-				);
-				fs.writeFile(
-					`${dir}/${newName}.php`,
-					replaced3,
-					'utf-8',
-					function (err) {
-						if (err) throw err;
-						console.log(
-							`👑👑\x1b[32m Successfully duplicated the php file! 👑👑`
-						);
-					}
-				);
-			}
-		);
-	}
-	if (fs.existsSync(`${srcPathVue}/${blockName}.vue`)) {
-		fs.readFile(
-			`${srcPathVue}/${blockName}.vue`,
-			'utf-8',
-			(err, contents) => {
-				if (err) throw err;
-				const replaced = contents.replaceAll(
-					`${blockName}`,
-					`${newName}`
-				);
-				fs.writeFile(
-					`${dir}/${newName}.vue`,
-					replaced,
-					'utf-8',
-					function (err) {
-						if (err) throw err;
-						console.log(
-							`👑👑\x1b[32m Successfully duplicated the vue file! 👑👑`
-						);
-					}
-				);
-			}
-		);
-	}
-	const handleError = (err) => console.error(err);
-	const compressDone = () => {
-		fs.rmSync(`../../../../exported-blocks/${newName}`, {recursive: true});
-	};
-
-	setTimeout(() => {
-		compressing.zip
-			.compressDir(`${dir}`, `${dir}.zip`)
-			.then(compressDone)
-			.catch(handleError);
-	}, 100);
+	compressing.zip
+		.compressDir(`${dir}`, `${dir}.zip`)
+		.then(() => {
+			fs.rmSync(`${destPath}/${newName}`, {recursive: true});
+			console.log(`\x1b[32m 🎉 Zip created at ${dir}.zip \x1b[0m`);
+		})
+		.catch((err) => console.error(err));
 }
